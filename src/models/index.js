216fc17a25b0d15b398b99bbfc986a1c0caf5ca5
Ian Jairo <41593870+IanJairo@users.noreply.github.com>
2025-06-09 17:24:00 -0300

    feat: implementa API inicial de verificação e registro de passageiros

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,17 @@
+// src/models/index.js
+
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+
+// AQUI ESTÁ A CORREÇÃO PRINCIPAL:
+// O caminho agora sobe dois níveis (de /src/models para a raiz)
+// e aponta para o nosso arquivo config/database.js
+const config = require(__dirname + '/../../config/database.js')[env];
 const db = {};
 
 let sequelize;
@@ -19,12 +24,7 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
